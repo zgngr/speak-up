87e6c679f3d1d5ce57a96a26dbf2d133c13bcd94
Ozgun Gor <zgn.gor@gmail.com>
2024-02-17 22:59:24 +0300

    restore point 4

--- app.py
+++ app.py
@@ -4,13 +4,11 @@ from openai import OpenAI
 from pydub import AudioSegment
 from difflib import Differ
 
-client = OpenAI(api_key="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")
-
-def speech_to_text(speech):
+def speech_to_text(client, speech):
     transcript = client.audio.transcriptions.create(model="whisper-1", file=speech, response_format="text")
     return transcript
 
-def improve_speech(speech):
+def improve_speech(client, speech):
     prompt = [
         {"role": "assistant", "content": 'You are a english expert. Improve my text. Make it simple, shorter and easy to understand.'}, 
         {"role": "user", "content": speech}
@@ -27,7 +25,6 @@ def diff_texts(text1, text2):
     ]
 
 with gr.Blocks() as ui:
-
     with gr.Column():
         welcome =gr.Markdown(
         """
@@ -36,31 +33,37 @@ with gr.Blocks() as ui:
         """)
 
     with gr.Column():
+        api_key = gr.Textbox(label="OpenAI API key", placeholder="Enter you key...", lines=1, max_lines=1)
         audio = gr.Audio(sources=["microphone"], type="filepath", label="Record your speech", max_length=20)
         run_btn = gr.Button("Speak UP!")
     
     with gr.Column(visible=False) as results:
         text_original_transcript = gr.Text(label="Original transcript:", lines=3)
         text_improved_transcript = gr.Text(label="Improved transcript:", lines=3)
-        text_diff =  gr.HighlightedText(label="Diff", combine_adjacent=True, show_legend=True, color_map={"+": "red", "-": "green"})
+        text_diff = gr.HighlightedText(label="Diff", combine_adjacent=True, show_legend=True, color_map={"+": "red", "-": "green"})
 
-    def run(speech):
+    def run(api_key, speech):
         #### input validation
+        if not api_key:
+            raise gr.Error("Missing API Key!")
+
         if not speech:
             raise gr.Error("Record something!")
 
-        audio_segment = AudioSegment.from_file(speech)
+        #### saving speech
+        file_path = "speech.wav"
+        shutil.copyfile(speech, file_path)
+
+        audio_segment = AudioSegment.from_file(file_path)
         duration_seconds = len(audio_segment) / 1000
         if duration_seconds > 20:
             raise gr.Error("Audio input should not be longer than 20 seconds.")
     
-        #### saving speech
-        file_path = "speech.wav"
-        shutil.copyfile(speech, file_path)
-    
+        gr.Info("Starting process...")
         #### OpenAI calls
-        original_transcript = speech_to_text(open(file_path, "rb"))
-        improved_transcript = improve_speech(original_transcript)
+        client = OpenAI(api_key=api_key)
+        original_transcript = speech_to_text(client, open(file_path, "rb"))
+        improved_transcript = improve_speech(client, original_transcript)
     
         #### diff
         diff = diff_texts(original_transcript, improved_transcript)
@@ -72,7 +75,7 @@ with gr.Blocks() as ui:
             text_diff: diff
         }
 
-    def visible_components():
+    def show_components():
         return {
             results: gr.Column(visible=True),
             text_original_transcript: "", 
@@ -80,7 +83,7 @@ with gr.Blocks() as ui:
             text_diff: ""
         }
 
-    def reset():
+    def hide_components():
         return {
             results: gr.Column(visible=False),
             text_original_transcript: "", 
@@ -88,18 +91,11 @@ with gr.Blocks() as ui:
             text_diff: ""
         }
     
-    run_btn.click(
-        visible_components, 
-        inputs=None, 
-        outputs=[results, text_original_transcript, text_improved_transcript, text_diff]
-    ).then(
-        run, 
-        inputs=audio, 
-        outputs=[results, text_original_transcript, text_improved_transcript, text_diff]
-    )
+    #### events
 
-    audio.clear(reset, None, outputs=[results, text_original_transcript, text_improved_transcript, text_diff])
+    run_btn.click(run, inputs=[api_key, audio], outputs=[results, text_original_transcript, text_improved_transcript, text_diff])
 
+    audio.clear(hide_components, None, outputs=[results, text_original_transcript, text_improved_transcript, text_diff])
 
 if __name__ == "__main__":
     ui.launch()
